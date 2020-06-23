a1a4fedb15457f18a04c59d06948158f235dcfcb
Dominique Masena <dominique.masena@live.ca>
2020-06-23 01:20:56 +0000

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual / expected === 1) {
+    console.log("🛑🛑🛑 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("✅✅✅ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+assertEqual(1, 1);
+  
