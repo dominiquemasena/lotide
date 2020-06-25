6fd610bcd08e33b16c0a09298b9afdcbe1cfe109
Dominique Masena <dominique.masena@live.ca>
2020-06-25 04:39:31 +0000

    countletters.js

---
+++ countletters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual / expected === 1) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function (string){
+  let output = {};
+  let newString = string.split(" ").join("");
+
+for (let i of newString) {
+  if (output[i]){
+    output[i] += 1;
+  } else {
+    output[i] = 1;
+  }
+}
+return output;
+}
+console.log(countLetters("Dominique"));
