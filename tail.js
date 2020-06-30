bf51237f9571345c88999d0d53fdfa343d564277
Dominique Masena <dominique.masena@live.ca>
2020-06-30 12:22:49 +0000

    tail.js

--- tail.js
+++ tail.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual / expected === 1) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-let arguments = [];
-
 const tail = function(arr) {
+  let arguments = [];
   arguments = arr.slice(1);
   return arguments;
 };
-assertEqual(tail([5,6,7]), 5);
+
+module.exports = tail;
