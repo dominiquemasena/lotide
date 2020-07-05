ba86a2a7b06805e004c4b68356ffdbbb596dc043
Dominique Masena <dominique.masena@live.ca>
2020-07-05 22:25:41 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const middle = function(arr) { 
   if (arr.length >= 3) {
   return arr[Math.round((arr.length - 1) / 2)]}
@@ -6,34 +8,6 @@ const middle = function(arr) {
 };
 
 
-const assertEqual = function(actual, expected) {
-  if (actual / expected === 1) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+assertEqual(middle([1, 2, 3]), 2);
 
-
-function assertArraysEqual(arr1, arr2) {
-  let arr1 = [];
-  let arr2 = [];
-  if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
-
-function eqArrays(arr1, arr2) {
-  if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-
-
-
-module.exports = middle;
+module.exports = middle; 
