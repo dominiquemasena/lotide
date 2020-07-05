1e389b178af0910975599fa3f17fde676536aad3
Dominique Masena <dominique.masena@live.ca>
2020-07-05 21:02:36 +0000

    eqarrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,5 @@
 const assertEqual = require('./assertEqual');
+
 function eqArrays(arr1, arr2) {
   if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
     return true;
@@ -9,9 +10,5 @@ function eqArrays(arr1, arr2) {
 }
 
 
-assertEqual((eqArrays([1, 2, 3], [1, 2, 3])), true);
-assertEqual((eqArrays([1, 2, 3], [3, 2, 1])), false);
-assertEqual((eqArrays(["1", "2", "3"], ["1", "2", "3"])), true);
-assertEqual((eqArrays(["1", "2", "3"], ["1", "2", 3])), false);
 
 module.exports = eqArrays;
