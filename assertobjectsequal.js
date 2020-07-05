8dea8347be47f9c58fed7cfac07b0c33aab63e91
Dominique Masena <dominique.masena@live.ca>
2020-07-05 00:46:11 +0000

    assertobjectsequal.js

---
+++ assertobjectsequal.js
@@ -0,0 +1,37 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  
+  if (eqObjects(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+
+const eqObjects = function(object1, object2) {
+  for (let key in object1) {
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+      return false;
+    }
+
+    if (typeof object1[key] !== typeof object2[key]) {
+      return false;
+    }
+
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    }
+
+    if ((typeof object1[key] === typeof object2[key]) && (object1[key] !== object2[key])) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(assertObjectsEqual(ab, ba), true);
