ef18ab216cdf4eb2d695a1f07b34d7728f3a3858
Dominique Masena <dominique.masena@live.ca>
2020-07-20 17:59:50 +0000

    head.js

--- head.js
+++ head.js
@@ -1,16 +1,55 @@
-const assertEqual = require('./assertEqual');
-
-
-const head = function(arr) {
-  return arr[0];
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
+
+
+const head = function(array){
+  for (let item of array){
+    return array[0];
+  }  
+}
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 
 
-module.exports = head;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const assertEqual = require('./assertEqual');
+
+
+// const head = function(arr) {
+//   return arr[0];
+// };
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+
+// module.exports = head;
 
 
 
