2d2879266c9f2dec0452fcf4bdcad6da6f04ae2e
Dominique Masena <dominique.masena@live.ca>
2020-07-22 10:39:30 +0000

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,30 @@
-const assertEqual = require('./assertEqual');
-
-function eqArrays(arr1, arr2) {
-  if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
-    return true;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-  return false;
+};
 
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true; 
 }
 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
-module.exports = eqArrays;
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
