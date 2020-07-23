cc31d864235e1a07b7ae2225cc26ce730c8106ef
Dominique Masena <dominique.masena@live.ca>
2020-07-23 04:07:56 +0000

    letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,14 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true; 
+}
+
+
 const letterPositions = function(sentence) {
   const results = {};
- for (let i in sentence){
-   if (sentence[i] !== " "){
-     if (results[sentence[i]]) {
-       results[sentence[i]].push(Number(i));
+  for (let letter in sentence) {
+  if (sentence[letter] !== " ") {
+    if (results[sentence[letter]]) {
+       results[sentence[letter]].push(Number(letter));
      } else {
-       results[sentence[i]] = [Number(i)];
+       results[sentence[letter]] = [Number(letter)];
      }
    }
  }
  return results;
 };
+
+
 console.log(letterPositions("lighthouse in the house"));
+// assertArraysEqual(letterPositions("hello"), [1]);
+
+//create test
