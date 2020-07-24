c06cad2fc619bb1974b2597f182d5fc1fc0f7e10
Dominique Masena <dominique.masena@live.ca>
2020-07-24 23:34:15 +0000

    head.js

--- head.js
+++ head.js
@@ -18,19 +18,7 @@ assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 
 
-// const assertEqual = require('./assertEqual');
 
-
-// const head = function(arr) {
-//   return arr[0];
-// };
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-
-
-// module.exports = head;
 
 
 
