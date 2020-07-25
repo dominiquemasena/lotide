0c3f4a7361aafde203b9a048c4f54a0550730e9d
Dominique Masena <dominique.masena@live.ca>
2020-07-25 19:51:37 +0000

    takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,19 +1,50 @@
-const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = function(array1, array2) {
 
-const takeUntil = function(array, callback) {
-  const results = [];
-  
-  for (let item of array) {
-    if (!callback.includes(item)) {
-      results.push(item);
-    } else {
-      return results;
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-};
+  return true; 
+}
 
 
 
-assertArraysEqual(takeUntil([1, 2, 3, 1, 2, 3], [3]), [1, 2]);
-assertArraysEqual(takeUntil([6, 7, 8, 9], [8]), [6, 7]);
-assertArraysEqual(takeUntil(["1", "2", "3"], ["3"]), ["1", "2"]);
+function assertArraysEqual(actual, expected) {
+  const arrayEqual = eqArrays(actual, expected);
+   if (!arrayEqual) {
+     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+   } else {
+     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);;
+ }
+}
+
+const takeUntil = function(array, callback) {
+  let untilarr = [];
+  for (let item of array) {
+    if (callback(item) !== true) {
+      untilarr.push(item);
+
+    } else {
+      return untilarr;
+    }
+  }
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+
+assertArraysEqual(takeUntil([1, 2, 3, 1, 2, 3], x => x > 2),[1, 2]);
