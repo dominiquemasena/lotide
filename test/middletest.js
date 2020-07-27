1ea52ed8179d9b6b04638b9c69f5edcecffd9f09
Dominique Masena <dominique.masena@live.ca>
2020-07-27 23:49:56 +0000

    middletest.js

--- test/middletest.js
+++ test/middletest.js
@@ -1,10 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
-// const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe("#middle", () => {
+  it("returns [54, 45] for [11, 23, 36, 54, 45, 76, 97, 88]", () => {
+    assert.deepEqual(middle([11, 23, 36, 54, 45, 76, 97, 88]), [54, 45]);
+  });
 
-// console.log("calling middle");
-const m = middle([11, 23, 36, 54, 45, 76, 97, 88]);
-// console.log("m =", m);
-assertArraysEqual(m, [54, 45]);
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  // it("returns '5' for ['5']", () => {
+  //   assert.strictEqual(head(['5']), '5'); 
+  // });
+});
+
+// const assert = require('chai').assert;
+// const middle   = require('../middle');
 
