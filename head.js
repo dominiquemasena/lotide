5332ec3a2543e1c378b69d66dc169402c062c4ce
Dominique Masena <dominique.masena@live.ca>
2020-07-27 17:34:35 +0000

    head.js

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 const head = function(array){
@@ -13,8 +7,8 @@ const head = function(array){
   }  
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = assertEqual;
+module.exports = head;
 
 
 
