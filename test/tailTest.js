ec0e1a1626704755c61820d5c810cfc23dbc11e9
Dominique Masena <dominique.masena@live.ca>
2020-07-27 23:49:16 +0000

    tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,36 @@
 const assert = require('chai').assert;
-const tail   = require('../tail');
+const tail = require('../tail');
 
 describe("#tail", () => {
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
   it("returns [2, 3] for [1, 2, 3]", () => {
     assert.deepEqual(tail([1, 2, 3]), [2, 3]);
   });
+
+  // it("returns '5' for ['5']", () => {
+  //   assert.strictEqual(head(['5']), '5'); 
+  // });
 });
+// const assertArraysEqual = require('../assertArraysEqual');
+// const tail   = require('../tail');
+
+
+
+// const array1 = [5,6,7]; 
+// const array2 = tail(array1); 
+
+// // assertEqual(array1, array1);
+
+// assertArraysEqual(tail([5,6,7]), [6,7]);
+// // assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+// // describe("#tail", () => {
+
+// // });
