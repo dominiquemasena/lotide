87604ac084e8581f77dbd9779782340463e9b04a
Dominique Masena <dominique.masena@live.ca>
2020-07-27 20:35:26 +0000

    middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,6 @@
-function assertArraysEqual(actual, expected) {
-  const arrayEqual = eqArrays(actual, expected);
-   if (!arrayEqual) {
-     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-   } else {
-     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);;
- }
-}
+// const assertArraysEqual = require('./assertArraysEqual');
+// const eqArrays = require('./eqArrays');
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true; 
-}
 
 
 const middle = function(array) { 
@@ -36,16 +19,18 @@ const middle = function(array) {
   };
 
 
-// 0, 1, 2, 3, 4, 5, 6, 7
-assertArraysEqual(eqArrays(middle([11, 23, 36, 54, 45, 76, 97, 88]), [54, 45]), true);
-console.log("middle([45, 24])", middle([45, 24])) // []
-console.log("middle([1, 2, 3, 4, 5, 6, 7])", middle([1, 2, 3, 4, 5, 6, 7])); // [4]
-console.log("middle([11, 23, 36, 54, 45, 76, 97, 88])", middle([11, 23, 36, 54, 45, 76, 97, 88])); // [54, 45]
+// // 0, 1, 2, 3, 4, 5, 6, 7
+// assertArraysEqual(eqArrays(middle([11, 23, 36, 54, 45, 76, 97, 88]), [54, 45]), true);
+// console.log("middle([45, 24])", middle([45, 24])) // []
+// console.log("middle([1, 2, 3, 4, 5, 6, 7])", middle([1, 2, 3, 4, 5, 6, 7])); // [4]
+// console.log("middle([11, 23, 36, 54, 45, 76, 97, 88])", middle([11, 23, 36, 54, 45, 76, 97, 88])); // [54, 45]
 
 
 
 
-
+// module.exports = eqArrays;
+module.exports = middle;
+// module.exports = assertArraysEqual;
 
 
 
